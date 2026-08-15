693d49b327ca482d16341b17ad7be847f20e0d7e
Dhananjay Pal <dhananjay.dk8858@gmail.com>
2026-08-15 12:59:23 +0530

    Time: 15 ms (10.86%), Space: 8.9 MB (83.15%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -1,19 +1,30 @@
 class Solution {
-    int MOD=1e9+7;
-private:
-    int fun(string &s,int idx,char prev,map<pair<int,char>,int>&dp){
-        if(idx>=s.size())return 1;
-        if(dp.find({idx,prev})!=dp.end())return dp[{idx,prev}];
-        int a=fun(s,idx+1,prev,dp)%MOD;
-        if(s[idx]!=prev)a=(a%MOD+fun(s,idx+1,s[idx],dp)%MOD)%MOD;
-        return dp[{idx,prev}]=a;
-    }
+    int MOD = 1e9 + 7;
+    // private:
+    //     int fun(string &s,int idx,char prev,map<pair<int,char>,int>&dp){
+    //         if(idx>=s.size())return 1;
+    //         if(dp.find({idx,prev})!=dp.end())return dp[{idx,prev}];
+    //         int a=fun(s,idx+1,prev,dp)%MOD;
+    //         if(s[idx]!=prev)a=(a%MOD+fun(s,idx+1,s[idx],dp)%MOD)%MOD;
+    //         return dp[{idx,prev}]=a;
+    //     }
 public:
     int distinctSubseqII(string s) {
-        int n=s.size();
-        map<pair<int,char>,int>dp;
-        int ans=fun(s,0,'#',dp);
-        return (ans-1+MOD)%MOD;
-        
+        int n = s.size();
+
+        vector<int> curr(27, 0), next(27, 1);
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prev = 0; prev <= 26; prev++) {
+                int a = next[prev];
+                if (prev == 26 || s[idx] - 'a' != prev)
+                    a = (a % MOD + next[s[idx] - 'a']) % MOD;
+                curr[prev] = a;
+            }
+            next = curr;
+        }
+
+        int ans = curr[26];
+        return (ans - 1 + MOD) % MOD;
     }
 };
